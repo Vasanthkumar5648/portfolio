109e31698b7d92b0e6dfca9687da6a6f8f7ea8eb
Vasanthkumar5648 <56169796+Vasanthkumar5648@users.noreply.github.com>
2025-05-17 22:52:22 +0530

    Update scr.py

--- scr.py
+++ scr.py
@@ -18,8 +18,8 @@ def load_image_from_url(url):
     return img
 
 # GitHub image URLs (replace with your actual image URLs)
-PROFILE_IMAGE_URL = "http://raw.github.com/Vasanthkumar5648/portfolio/main/image.jpg%20(2)%20(1).jpg"
-ABOUT_IMAGE_URL = "https://raw.http://github.com/Vasanthkumar5648/portfolio/main/image.jpg%20(2)%20(1).jpg.jpg"
+PROFILE_IMAGE_URL ="https://raw.github.com/Vasanthkumar5648/portfolio/main/image.jpg%20(2)%20(1).jpg"
+ABOUT_IMAGE_URL ="https://raw.http://github.com/Vasanthkumar5648/portfolio/main/image.jpg%20(2)%20(1).jpg"
 
 # Custom CSS for black sidebar with white text
 st.markdown("""
